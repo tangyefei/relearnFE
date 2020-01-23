2e5fe97c18929f5416914a3ec3389cfb35da7352
tangyefei <tyfccsu@gmail.com>
2020-01-23 23:02:36 +0800

    vue optimize

--- src/js/vue/parser.js
+++ src/js/vue/parser.js
@@ -127,7 +127,6 @@ parseHTML(template, {
   }
 })
 
-console.log(root);
 
 function parseText(text) {
   text = text.trim();
@@ -135,21 +134,78 @@ function parseText(text) {
   if(!text) return;
 
   var re = /\{\{\w+\}\}/g;
-  var match;
-  var tokens = [];
-  var lastIndex = 0;
 
-  while((match = re.exec(text)) != null) {
-    if(match.index > 0) tokens.push('"' + text.substring(lastIndex, match.index)  + '"');
-    tokens.push('_s(' + match[0].substring(2,match[0].length-2) + ')')
-    lastIndex = re.lastIndex;
-    // console.log(lastIndex);
+  if(re.test(text)) {
+    var match;
+    var tokens = [];
+    var lastIndex = 0;
+  
+    while((match = re.exec(text)) != null) {
+      if(match.index > 0) tokens.push('"' + text.substring(lastIndex, match.index)  + '"');
+      tokens.push('_s(' + match[0].substring(2,match[0].length-2) + ')')
+      lastIndex = re.lastIndex;
+      // console.log(lastIndex);
+    }
+    if(lastIndex < text.length) {
+      tokens.push('"' + text.substring(lastIndex) + '"');
+    }
+    // console.log(tokens);
+    return tokens.join('+')
   }
-  if(lastIndex < text.length) {
-    tokens.push('"' + text.substring(lastIndex) + '"');
-  }
-  // console.log(tokens);
-  return tokens.join('+')
+
 }
 // console.log(parseText('haha {{name}} receive offer worth {{amount}}'));
 // console.log(parseText("{{owner}}'s blog"));
+
+
+function isStatic(node) {
+  if(node.type == 2) {
+    return false;
+  } else if(node.type == 3) {
+    return true;
+  } else {
+    // node.type == 1 需要区分普通个元素还是有 v-if v-pre 等各种directive的元素，这里先都统一返回为true，因为还没实现directive
+    return true;
+  }
+}
+
+function markStatic(parent) {
+  parent.static = isStatic(parent);
+  if(parent.type == 1 && parent.children) {
+    for (let i = 0; i < parent.children.length; i++) {
+      const child = parent.children[i];
+      child.static = isStatic(child);
+      markStatic(child);
+      if(!child.static) {
+        parent.static = false;
+      }
+    }
+  }
+}
+
+function markRootStatic(parent) {
+  if(parent.static) {
+    parent.staticRoot = true;
+    return;
+  } else {
+    parent.staticRoot = false;
+  }
+  if(parent.children) {
+    for (let i = 0; i < parent.children.length; i++) {
+      const child = parent.children[i];
+      markRootStatic(child);
+    }
+  }
+}
+
+function optimize(root) {
+  if(!root) return;
+
+  markStatic(root);
+  markRootStatic(root);
+}
+
+optimize(root);
+
+
+console.log(root);
