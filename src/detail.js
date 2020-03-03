3b03faa967069eb3422b5b033b5b67f3a8baa04a
tangyefei <tyfccsu@gmail.com>
2020-03-03 21:06:26 +0800

    backup

--- src/detail.js
+++ src/detail.js
@@ -4,14 +4,13 @@ import "css/base.css";
 import "css/detail.css";
 import PagePen from 'page-pen';
 
-
 var s = window.location.search;
 var matches = s.match(/id=.+/g);
 if(matches && matches.length) {
   Request.getArticle(s.slice(1).split('=')[1], function (resp) {
     if (resp.code == 1) {
       Render.renderDetail(resp.body);
-      new PagePen(document.body);
+      //new PagePen(document.body);
     }
   })
 } else {
